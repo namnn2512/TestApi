11ab0e7c24b0827e5154c825bfa9720b2b524836
Linh Hoang <linh.cmu@gmail.com>
2017-06-26 22:06:57 +0700

    ass

--- app.js
+++ app.js
@@ -4,35 +4,46 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var express = require('express');
 var app = express();
 var http = require('http');
+var https = require('https');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var bittrex = require('node.bittrex.api');
 var autobahn = require('autobahn');
 var wsuri = "wss://api.poloniex.com";
+var bodyParser = require("body-parser");
 
 var connection = new autobahn.Connection({
     url: wsuri,
     realm: "realm1"
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/bower_components'));
 app.get('/', function (req, res, next) {
-    // var url = "https://poloniex.com/public?"+"command=returnOrderBook"+"&currencyPair=BTC_NXT"+"&depth=10"
-
-    // https.get(url, function(res){
-    // var body = '';
-
-    // res.on('data', function(chunk){
-    //     body += chunk;
-    // });
-
-    // res.on('end', function(){
-    //     console.log("Got a response: ", body);
-    // });
-    // }).on('error', function(e){
-    //       console.log("Got an error: ", e);
-    // });
     res.sendFile(__dirname + '/index.html');
+});
+app.post('/tradeOB',function(req,res,next) {
+    var command = req.body.command;
+    var currencyPair = req.body.currencyPair;
+    var depth = req.body.depth;
+    console.log(command+currencyPair+depth);
+    var url = "https://poloniex.com/public?command="+command+"&currencyPair="+currencyPair+"&depth="+depth
+    var body = '';
+    https.get(url, function(response){
+        response.on('data', function(chunk){
+            body += chunk;
+        });
+
+        response.on('end', function(){
+            console.log("Got a response: ", body);
+            res.json(body);
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+          res.json(e);
+    });
 });
 
 var port = 3000;
